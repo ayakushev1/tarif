8461db2c447fe32f46a8d2cfb78d86f385ce1e96
ayakushev1 <ayakushev@rambler.ru>
2014-06-14 10:28:42 +0400

    After adding category_group optimization

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,14 @@
 Dir[Rails.root.join("db/seeds/definitions/*.rb")].sort.each { |f| require f }
 
 %w{
-  customer/calls
+ users categories tarif_classes parameters service/categories service/category_tarif_classes service/category_groups 
+ tarif_lists price_lists price/standard_formulas price/formulas customer/service relations customer/calls service/priorities
 }.each do |part|
   require File.expand_path(File.dirname(__FILE__))+"/seeds/#{part}.rb"
 end
 
-#Dir[Rails.root.join("db/seeds/tarif_tests/mts/tarifs/smart_plus.rb")].each { |f| require f }
-#Dir[Rails.root.join("db/seeds/tarifs/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("db/seeds/tarif_tests/mts/tarifs/smart_plus.rb")].each { |f| require f }
+Dir[Rails.root.join("db/seeds/tarifs/**/*.rb")].each { |f| require f }
 
 #@tc.load_repositories
 
