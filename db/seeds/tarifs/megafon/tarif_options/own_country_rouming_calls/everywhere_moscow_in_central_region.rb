9d1e649dc44fcb4133e87969199eb8a1f9ac55b3
ayakushev1 <ayakushev@rambler.ru>
2014-09-29 03:44:37 +0400

    dddddd

--- db/seeds/tarifs/megafon/tarif_options/own_country_rouming_calls/everywhere_moscow_in_central_region.rb
+++ db/seeds/tarifs/megafon/tarif_options/own_country_rouming_calls/everywhere_moscow_in_central_region.rb
@@ -8,7 +8,7 @@
     :other_tarif_priority => {:lower => [], :higher => []},
     :prerequisites => [],
     :forbidden_tarifs => {:to_switch_on => [], :to_serve => []},
-    :multiple_use => true
+    :multiple_use => false
   } } )
 
 #Параметры опции задаются в описании самого тарифа
