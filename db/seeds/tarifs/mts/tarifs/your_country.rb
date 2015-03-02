c2b4b283e258f5741c5e36035bcbfcb0ecd4fc33
ayakushev1 <ayakushev@rambler.ru>
2015-03-02 13:36:52 +0300

    correcting tarifs

--- db/seeds/tarifs/mts/tarifs/your_country.rb
+++ db/seeds/tarifs/mts/tarifs/your_country.rb
@@ -138,6 +138,10 @@
   category = {:name => '_sctcg_own_home_regions_sms_to_sic_country', :service_category_rouming_id => _own_and_home_regions_rouming, :service_category_calls_id => _sms_out, :service_category_geo_id => _service_to_mts_sic}
   @tc.add_one_service_category_tarif_class(category, {}, {:calculation_order => 0,:standard_formula_id => _stf_price_by_count_volume_item, :price => 2.0})
 
+#Own and home regions, sms, outcoming, to_europe
+  category = {:name => '_sctcg_own_home_regions_sms_to_europe', :service_category_rouming_id => _own_and_home_regions_rouming, :service_category_calls_id => _sms_out, :service_category_geo_id => _service_to_mts_europe}
+  @tc.add_one_service_category_tarif_class(category, {}, {:calculation_order => 0,:standard_formula_id => _stf_price_by_count_volume_item, :price => 5.25})
+
 #Own and home regions, sms, outcoming, to_not_own_country, others (отрегулировано порядоком вычисления, а не категорией)
   category = {:name => '_sctcg_own_home_regions_sms_to_other_countries', :service_category_rouming_id => _own_and_home_regions_rouming, :service_category_calls_id => _sms_out, :service_category_geo_id => _service_to_mts_other_countries}
   @tc.add_one_service_category_tarif_class(category, {}, {:calculation_order => 0,:standard_formula_id => _stf_price_by_count_volume_item, :price => 5.25})
