635ffa038a3e695c99f095b78d9ac17ae3de3117
ayakushev1 <ayakushev@rambler.ru>
2015-03-21 07:06:01 +0300

    merged demo/calls with customer/calls

--- app/helpers/application_helper/authenticity_and_authorization.rb
+++ app/helpers/application_helper/authenticity_and_authorization.rb
@@ -118,6 +118,7 @@ module ApplicationHelper::AuthenticityAndAuthorization
     def controller_has_free_public_url?
 #      raise(StandardError, [controller_name, action_name])
       ((controller_name == 'demo/home' or controller_name == 'home') and ['index', 'short_description', 'detailed_description'].include?(action_name)) or
-      ((controller_path == 'content/articles') and ['show', 'index', 'call_statistic', 'detailed_results'].include?(action_name))
+      ((controller_path == 'content/articles') and ['show', 'index', 'call_statistic', 'detailed_results'].include?(action_name)) or
+      ((controller_path == 'customer/calls') and ['index', 'set_calls_generation_params', 'set_default_calls_generation_params', 'generate_calls'].include?(action_name))
     end
 end
