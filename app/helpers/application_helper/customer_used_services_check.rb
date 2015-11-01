db845d1a276ed9c61ff43376d558413d60747ea1
ayakushev1 <ayakushev@rambler.ru>
2015-11-01 22:53:56 +0300

    add false return in case nill current user in customer_used_services_check

--- app/helpers/application_helper/customer_used_services_check.rb
+++ app/helpers/application_helper/customer_used_services_check.rb
@@ -31,31 +31,38 @@ module ApplicationHelper::CustomerUsedServicesCheck
   end
 
   def customer_has_free_calls_modelling_trials?
+    return false if !current_user
     (customer_info and customer_info['calls_modelling_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_history_parsing_trials?
+    return false if !current_user
     (customer_info and customer_info['calls_parsing_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_tarif_optimization_trials?
+    return false if !current_user
     (customer_info and customer_info['tarif_optimization_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_tarif_recalculation_trials?
+    return false if !current_user
     (customer_info and (customer_info['tarif_recalculation_count'] || 0) < 1 ) ? false : true
   end
   
   def customer_has_calls_loaded?
+    return false if !current_user
     (customer_info and customer_info['has_calls_loaded'] == true ) ? true : false
   end
   
   def customer_has_tarif_optimized?
+    return false if !current_user
 #    raise(StandardError)
     (customer_info and customer_info['has_tarif_optimized'] == true ) ? true : false
   end
   
   def customer_paid_trials?
+    return false if !current_user
     (customer_info and customer_info['paid_trials'] == true ) ? true : false
   end
       
