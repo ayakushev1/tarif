84271fa9cdc73390d6b667500e9a7ebc717a7969
ayakushev1 <ayakushev@rambler.ru>
2015-11-03 03:04:18 +0300

    ddd

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -3,12 +3,16 @@ if ['development', 'production'].include?(Rails.env)
 
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
-#  Rails.application.middleware.delete(Rack::MiniProfiler)
-#  Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+  
+  if ['development', 'production'].include?(Rails.env)
+    Rails.application.middleware.delete(Rack::MiniProfiler)
+    Rails.application.middleware.insert_after(Rack::Deflater, Rack::MiniProfiler)
+  end
   
   Rack::MiniProfiler.config.position = 'right'
-  Rack::MiniProfiler.config.start_hidden = false
+  Rack::MiniProfiler.config.start_hidden = true
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+  Rack::MiniProfiler.config.disable_caching = false
   
 end
 
