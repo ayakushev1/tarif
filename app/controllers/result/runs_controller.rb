6115a86b43d12ee413a44956f4db6f958847ecff
ayakushev1 <ayakushev@rambler.ru>
2015-11-09 05:06:34 +0300

    ddd

--- app/controllers/result/runs_controller.rb
+++ app/controllers/result/runs_controller.rb
@@ -1,4 +1,9 @@
 class Result::RunsController < ApplicationController
   include Result::RunsHelper
+  
+  def results
+#    @result_service_set = Result::ServiceSet.includes(:operator, :tarif).where(:run_id => run_id).order(:price).
+#      paginate(page: 1, :per_page => 10)
+  end
 
 end
