e7424ad88b8467eb1fd6ec92577a397b85a2537b
ayakushev1 <ayakushev@rambler.ru>
2015-11-13 02:18:25 +0300

    fix users/new path in registration and add name to allowed params of devise on sign_up

--- app/helpers/application_helper/devise_settings.rb
+++ app/helpers/application_helper/devise_settings.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper::DeviseSettings
 
   def configure_permitted_parameters
-#    devise_parameter_sanitizer.for(:sign_in) << :name #{ |u| u.permit(:name, :email, :password, :password_confirmation) }
-#    devise_parameter_sanitizer.for(:sign_up)  << :name #{ |u| u.permit(:name, :email, :password, :password_confirmation) }
-#    devise_parameter_sanitizer.for(:account_update)  << :name #{ |u| u.permit(:name, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_in) << :name #{ |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_up)  << :name #{ |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update)  << :name #{ |u| u.permit(:name, :password, :password_confirmation, :current_password) }
   end  
 
   def after_sign_in_path_for(resource)
