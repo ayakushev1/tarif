e95592bf348eb27fd1e0d3f1c539db03d4ba496f
ayakushev1 <ayakushev@rambler.ru>
2015-11-16 07:38:02 +0300

    ddd

--- app/controllers/customer/call_runs_controller.rb
+++ app/controllers/customer/call_runs_controller.rb
@@ -4,6 +4,7 @@ class Customer::CallRunsController < ApplicationController
   crudable_actions :all
   
   before_action :check_if_allowed_new_call_run, only: [:new, :create]
+  before_action :check_if_allowed_delete_call_run, only: [:destroy]
   
   def customer_call_runs
     create_tableable(Customer::CallRun.where(:user_id => current_or_guest_user_id))
@@ -11,12 +12,20 @@ class Customer::CallRunsController < ApplicationController
   
   def check_if_allowed_new_call_run
     message = "Вам не разрешено создавать более #{allowed_new_call_run(user_type)}  описаний"
-    redirect_to customer_call_runs_path, alert: message
+    redirect_to( customer_call_runs_path, alert: message) if !is_allowed_new_call_run?
+  end
+  
+  def check_if_allowed_delete_call_run
+    message = "Нельзя удалять последнее описание"
+    redirect_to( customer_call_runs_path, alert: message) if customer_call_runs_count < 2
   end
   
   def is_allowed_new_call_run?
-    existed_count = Customer::CallRun.where(:user_id => current_or_guest_user_id).count
-    existed_count < allowed_new_call_run(user_type) ? true : false
+    customer_call_runs_count < allowed_new_call_run(user_type) ? true : false
+  end
+  
+  def customer_call_runs_count
+    Customer::CallRun.where(:user_id => current_or_guest_user_id).count
   end
   
   def allowed_new_call_run(user_type = :guest)
