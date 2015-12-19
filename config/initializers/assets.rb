981191370e59c89a4adf0629838135c92efff75b
ayakushev1 <ayakushev@rambler.ru>
2015-12-19 06:33:33 +0300

    ss

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 #Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w(landing_zero_scripts.js wow.min.js)
+Rails.application.config.assets.precompile += %w(wow.min.js)
+Rails.application.config.assets.precompile += %w(landing_zero_scripts.js)
 Rails.application.config.assets.precompile += %w(landing_zero_styles.css )
