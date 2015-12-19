528c3444373727356c93386a5a574f63611c7336
ayakushev1 <ayakushev@rambler.ru>
2015-12-19 21:46:09 +0300

    change customer_used_service_check to allow all

--- app/helpers/application_helper/customer_used_services_check.rb
+++ app/helpers/application_helper/customer_used_services_check.rb
@@ -15,6 +15,7 @@ module ApplicationHelper::CustomerUsedServicesCheck
   end
   
   def customer_has_free_trials?(controller_name_1 = controller_name)
+    return true
     case 
     when controller_name_1 == 'calls'
       customer_has_free_calls_modelling_trials?
@@ -31,21 +32,25 @@ module ApplicationHelper::CustomerUsedServicesCheck
   end
 
   def customer_has_free_calls_modelling_trials?
+    return true
     return false if !current_or_guest_user
     (customer_info and customer_info['calls_modelling_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_history_parsing_trials?
+    return true
     return false if !current_or_guest_user
     (customer_info and customer_info['calls_parsing_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_tarif_optimization_trials?
+    return true
     return false if !current_or_guest_user
     (customer_info and customer_info['tarif_optimization_count'] < 1 ) ? false : true
   end
   
   def customer_has_free_tarif_recalculation_trials?
+    return true
     return false if !current_or_guest_user
     (customer_info and (customer_info['tarif_recalculation_count'] || 0) < 1 ) ? false : true
   end
