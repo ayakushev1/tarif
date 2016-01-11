0a7124c17fac612a8912e9f286240cc16a6f5ce9
ayakushev1 <ayakushev@rambler.ru>
2016-01-11 07:26:35 +0300

    aaa

--- app/controllers/result/service_sets_controller.rb
+++ app/controllers/result/service_sets_controller.rb
@@ -1,6 +1,6 @@
 class Result::ServiceSetsController < ApplicationController
-  include Result::ServiceSetsHelper
-  helper Result::ServiceSetsHelper
+  include Result::ServiceSetsHelper, Result::ServiceSets::ServiceCategoriesPresenter
+  helper Result::ServiceSetsHelper, Result::ServiceSets::ServiceCategoriesPresenter
 
   before_action :set_back_path, only: [:results, :result]
   
