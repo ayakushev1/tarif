9176ffc01d2f6146af18c8b655bfabc21ca80240
ayakushev1 <ayakushev@rambler.ru>
2016-01-11 12:21:19 +0300

    change description of comparison baskets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-#Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
+Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
 
 #Dir[Rails.root.join("db/seeds/seeds_for_tarif_autoload.rb")].each { |f| require f }
 
 #Dir[Rails.root.join("db/seeds/seeds_for_content_related.rb")].each { |f| require f }
 
-Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
-Customer::Stat::StatAndQuery.save_stat_function_collector
+#Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
+#Customer::Stat::StatAndQuery.save_stat_function_collector
--- db/seeds/comparison/groups.rb
+++ db/seeds/comparison/groups.rb
@@ -2,40 +2,40 @@ Comparison::Group.find_or_create_by(:id => 0).update(:name => 'Студенты'
 Comparison::Group.find_or_create_by(:id => 1).update(:name => 'Пенсионеры', :optimization_id => 0)
 
 
-Comparison::Group.find_or_create_by(:id => 10).update(:name => 'Малая корзина', :optimization_id => 1)
+Comparison::Group.find_or_create_by(:id => 10).update(:name => 'Малая корзина (80 мин звонков, 110 смс)', :optimization_id => 1)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 10, :call_run_id => 0)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 10, :call_run_id => 1)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 10, :call_run_id => 2)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 10, :call_run_id => 3)
 
 
-Comparison::Group.find_or_create_by(:id => 11).update(:name => 'Средняя корзина', :optimization_id => 1)
+Comparison::Group.find_or_create_by(:id => 11).update(:name => 'Средняя корзина (400 мин звонков, 240 смс, 1Гб интернета)', :optimization_id => 1)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 11, :call_run_id => 5)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 11, :call_run_id => 6)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 11, :call_run_id => 7)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 11, :call_run_id => 8)
 
-Comparison::Group.find_or_create_by(:id => 12).update(:name => 'Дорогая корзина', :optimization_id => 1)
+Comparison::Group.find_or_create_by(:id => 12).update(:name => 'Дорогая корзина (1200 мин звонков, 420 смс, 3Гб интернета)', :optimization_id => 1)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 12, :call_run_id => 10)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 12, :call_run_id => 11)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 12, :call_run_id => 12)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 12, :call_run_id => 13)
 
 
-Comparison::Group.find_or_create_by(:id => 15).update(:name => 'Малая корзина', :optimization_id => 2)
+Comparison::Group.find_or_create_by(:id => 15).update(:name => 'Малая корзина (80 мин звонков, 110 смс, 10% поездки по России)', :optimization_id => 2)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 15, :call_run_id => 20)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 15, :call_run_id => 21)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 15, :call_run_id => 22)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 15, :call_run_id => 23)
 
 
-Comparison::Group.find_or_create_by(:id => 16).update(:name => 'Средняя корзина', :optimization_id => 2)
+Comparison::Group.find_or_create_by(:id => 16).update(:name => 'Средняя корзина (400 мин звонков, 240 смс, 1Гб интернета, 10% поездки по России)', :optimization_id => 2)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 16, :call_run_id => 25)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 16, :call_run_id => 26)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 16, :call_run_id => 27)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 16, :call_run_id => 28)
 
-Comparison::Group.find_or_create_by(:id => 17).update(:name => 'Дорогая корзина', :optimization_id => 2)
+Comparison::Group.find_or_create_by(:id => 17).update(:name => 'Дорогая корзина (1200 мин звонков, 420 смс, 3Гб интернета, 10% поездки по России)', :optimization_id => 2)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 17, :call_run_id => 30)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 17, :call_run_id => 31)
 Comparison::GroupCallRun.find_or_create_by(:comparison_group_id => 17, :call_run_id => 32)
