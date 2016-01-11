b106c83a88299b402f992fbcad35d511cd570303
ayakushev1 <ayakushev@rambler.ru>
2016-01-11 09:42:48 +0300

    âââ

--- db/seeds/tarifs/beeline/common_services/international_rouming_internet_post.rb
+++ db/seeds/tarifs/beeline/common_services/international_rouming_internet_post.rb
@@ -16,14 +16,14 @@
 #SIC, Internet
 category = {:name => '_sctcg_bln_sic_internet', :service_category_rouming_id => _sc_bln_sic, :service_category_calls_id => _internet}
   @tc.add_one_service_category_tarif_class(category, {}, 
-    {:calculation_order => 0, :standard_formula_id => Price::StandardFormula::Const::MaxDurationMinuteForFixedPrice,  
+    {:calculation_order => 0, :standard_formula_id => Price::StandardFormula::Const::MaxSumVolumeMByteForFixedPriceIfUsed,  
       :formula => {:params => {:max_duration_minute => 40.0, :price => 200.0}, :window_over => 'day' } } )
   @tc.add_one_service_category_tarif_class(category, {}, {:calculation_order => 1, :standard_formula_id => Price::StandardFormula::Const::PriceBySumVolumeMByte, :formula => {:params => {:price => 5.0} } })  
 
 #Other countries, Internet
 category = {:name => '_sctcg_bln_other_countries_internet', :service_category_rouming_id => _sc_bln_other_world, :service_category_calls_id => _internet}
   @tc.add_one_service_category_tarif_class(category, {}, 
-    {:calculation_order => 0, :standard_formula_id => Price::StandardFormula::Const::MaxDurationMinuteForFixedPrice,  
+    {:calculation_order => 0, :standard_formula_id => Price::StandardFormula::Const::MaxSumVolumeMByteForFixedPriceIfUsed,  
       :formula => {:params => {:max_duration_minute => 40.0, :price => 200.0}, :window_over => 'day' } } )
   @tc.add_one_service_category_tarif_class(category, {}, {:calculation_order => 1, :standard_formula_id => Price::StandardFormula::Const::PriceBySumVolumeMByte, :formula => {:params => {:price => 5.0} } })  
 
