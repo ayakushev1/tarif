aacff370f554d8ec0ea4f9b3521495058df72d1d
ayakushev1 <ayakushev@rambler.ru>
2016-01-14 18:03:45 +0300

    sss

--- app/helpers/result/service_sets_helper.rb
+++ app/helpers/result/service_sets_helper.rb
@@ -21,11 +21,13 @@ module Result::ServiceSetsHelper
   end
   
   def identical_services
-    @identical_services ||= Result::ServiceSet.where(:run_id => run_id).pluck(:identical_services).flatten(2).uniq
+#    @identical_services ||= 
+    Result::ServiceSet.where(:run_id => run_id).pluck(:identical_services).flatten(2).uniq
   end
   
   def all_service_ids
-    @all_service_ids ||= Result::ServiceSet.where(:run_id => run_id).pluck(:service_ids).flatten.uniq
+#    @all_service_ids ||= 
+    Result::ServiceSet.where(:run_id => run_id).pluck(:service_ids).flatten.uniq
   end
 
   def zero_string_condition
@@ -39,23 +41,24 @@ module Result::ServiceSetsHelper
   def service_description(service_ids_1 = [])
     s_desc = {}
     service_ids = service_ids_1.map{|s| s.is_a?(String) ? s.split("_").map(&:to_i) : s }.flatten
-    @service_description ||= TarifClass.where(:id => service_ids).select(:id, :name, :features).each do |row|
+#    @service_description ||= 
+    TarifClass.where(:id => service_ids).select(:id, :name, :features).each do |row|
       s_desc[row[:id]] = {'service_name' => row[:name], 'service_http' => row[:features]['http']}
     end
     s_desc
   end
     
   def result_service_sets
-    return @result_service_sets if @result_service_sets
+#    return @result_service_sets if @result_service_sets
     options = {:base_name => 'service_sets', :current_id_name => 'service_set_id', :id_name => 'service_set_id', :pagination_per_page => 10}
-    @result_service_sets = 
+#    @result_service_sets = 
     create_tableable(Result::ServiceSet.includes(:operator, :tarif).where(:run_id => run_id).order(:price), options)
   end
   
   def results_service_sets
-    return @results_service_sets if @results_service_sets
+#    return @results_service_sets if @results_service_sets
     options = {:base_name => 'service_sets', :current_id_name => 'service_set_id', :id_name => 'service_set_id', :pagination_per_page => 10}
-    @results_service_sets = 
+#    @results_service_sets = 
     create_tableable(Result::ServiceSet.includes(:operator, :tarif).where(:run_id => session_filtr_params(results_select)['result_run_id']).order(:price), options)
   end
   
@@ -71,13 +74,15 @@ module Result::ServiceSetsHelper
   
   def result_service_categories
     options = {:base_name => 'service_categories', :current_id_name => 'service_category_name', :id_name => 'service_category_name', :pagination_per_page => 100}
-    @result_service_categories ||= create_tableable(Result::ServiceCategory.where(:run_id => run_id, :service_set_id => service_set_id, :service_id => service_id).
+#    @result_service_categories ||= 
+    create_tableable(Result::ServiceCategory.where(:run_id => run_id, :service_set_id => service_set_id, :service_id => service_id).
       where(zero_string_condition).order(:rouming_ids, :geo_ids, :partner_ids, :calls_ids, :fix_ids), options)
   end
   
   def result_agregates
     options = {:base_name => 'agregates', :current_id_name => 'aggregated_service_category_name', :id_name => 'service_category_name', :pagination_per_page => 100}
-    @result_agregates ||= create_tableable(Result::Agregate.where(:run_id => run_id, :service_set_id => service_set_id).
+#    @result_agregates ||= 
+    create_tableable(Result::Agregate.where(:run_id => run_id, :service_set_id => service_set_id).
       where(zero_string_condition).order(:rouming_ids, :geo_ids, :partner_ids, :calls_ids, :fix_ids), options)
   end
 
@@ -94,7 +99,8 @@ module Result::ServiceSetsHelper
     operator_id = Result::ServiceSet.where(:run_id => run_id, :service_set_id => service_set_id).pluck(:operator_id)[0]
     
     options = {:base_name => 'calls_stat', :current_id_name => 'calls_stat_category', :id_name => 'calls_stat_category', :pagination_per_page => 100}
-    @calls_stat ||= create_array_of_hashable(
+#    @calls_stat ||= 
+    create_array_of_hashable(
       Result::CallStat.where(:run_id => run_id, :operator_id => operator_id).first_or_create.calls_stat_array(calls_stat_options), options )
   end
    
@@ -118,7 +124,8 @@ module Result::ServiceSetsHelper
     
 #    raise(StandardError, [session_filtr_params_service_set_choicer['comparison_base'], comparison_base])
     options = {:base_name => 'comparison_service_sets', :current_id_name => 'global_category_id', :id_name => 'global_category_id', :pagination_per_page => 100}
-    @comparison_service_sets ||= create_array_of_hashable(
+#    @comparison_service_sets ||= 
+    create_array_of_hashable(
       Result::Agregate.compare_service_sets_of_one_run({run_id => service_set_ids}, [:price], comparison_options, comparison_base), options )
 #      raise(StandardError, @comparison_service_sets.model.collect {|row| row.keys }.flatten.uniq)
   end
