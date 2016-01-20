e813e411329584d6ee19d78d4367f06081111a92
ayakushev1 <ayakushev@rambler.ru>
2016-01-20 06:53:11 +0300

    fix correct autoincrement id for new records in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@ Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| requ
 
 #Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
 #Customer::Stat::StatAndQuery.save_stat_function_collector
+
+#to set correct id for new records
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
