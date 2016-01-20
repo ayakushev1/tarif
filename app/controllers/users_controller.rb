e10893c039d65d4c55e69602e6863b45ca7b2191
ayakushev1 <ayakushev@rambler.ru>
2016-01-20 06:56:17 +0300

    fix users index view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   include Crudable
   crudable_actions :all
+  include SavableInSession::Tableable
+  
+  def users
+    options = {:base_name => 'users', :current_id_name => 'user_id', :id_name => 'id', :pagination_per_page => 20}
+    create_tableable(User, options)
+  end
 
 
 
