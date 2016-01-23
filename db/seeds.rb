afd7019d68ac67d8bb0e8de676d972844bd2a40e
ayakushev1 <ayakushev@rambler.ru>
2016-01-23 22:47:11 +0300

    sss

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
+#Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
 
 #Dir[Rails.root.join("db/seeds/seeds_for_tarif_autoload.rb")].each { |f| require f }
 
 #Dir[Rails.root.join("db/seeds/seeds_for_content_related.rb")].each { |f| require f }
 
-Dir[Rails.root.join("db/seeds/seeds_users.rb")].each { |f| require f }
+Dir[Rails.root.join("db/seeds/seeds_for_users.rb")].each { |f| require f }
 
-Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
-Customer::Stat::StatAndQuery.save_stat_function_collector # возникает ошибка в Parameter если customer_calls empty
+#Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
+#Customer::Stat::StatAndQuery.save_stat_function_collector # возникает ошибка в Parameter если customer_calls empty
 
 #to set correct id for new records
 ActiveRecord::Base.connection.tables.each do |t|
