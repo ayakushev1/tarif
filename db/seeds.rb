62a2d383b06886fefe31422daa334ed3356aa506
ayakushev1 <ayakushev@rambler.ru>
2016-01-24 07:00:23 +0300

    sss

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@
 
 #Dir[Rails.root.join("db/seeds/seeds_for_content_related.rb")].each { |f| require f }
 
-Dir[Rails.root.join("db/seeds/seeds_for_users.rb")].each { |f| require f }
+#Dir[Rails.root.join("db/seeds/seeds_for_users.rb")].each { |f| require f }
 
 #Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
 #Customer::Stat::StatAndQuery.save_stat_function_collector # возникает ошибка в Parameter если customer_calls empty
