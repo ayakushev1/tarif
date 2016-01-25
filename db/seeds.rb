07d0d4251025cfc0d7121e18b5e539ba584552ed
ayakushev1 <ayakushev@rambler.ru>
2016-01-25 11:18:26 +0300

    change seed to choose from ENV['seed_option']

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-#Dir[Rails.root.join("db/seeds/seeds_for_users.rb")].each { |f| require f }
-
-#Dir[Rails.root.join("db/seeds/seeds_for_tarif_autoload.rb")].each { |f| require f }
-
-Dir[Rails.root.join("db/seeds/seeds_for_content_related.rb")].each { |f| require f }
-
-#Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
-#Customer::Stat::StatAndQuery.save_stat_function_collector # возникает ошибка в Parameter если customer_calls empty
-
-#Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
+case ENV["seed_option"]
+when "seeds_for_users"
+  Dir[Rails.root.join("db/seeds/seeds_for_users.rb")].each { |f| require f }
+when "seeds_for_tarif_autoload"
+  Dir[Rails.root.join("db/seeds/seeds_for_tarif_autoload.rb")].each { |f| require f }
+when "seeds_for_content_related"
+  Dir[Rails.root.join("db/seeds/seeds_for_content_related.rb")].each { |f| require f }
+when "seeds_for_tarif_related"
+  Dir[Rails.root.join("db/seeds/seeds_for_tarif_related.rb")].each { |f| require f }
+  Customer::Stat::StatAndQuery.save_stat_function_collector # возникает ошибка в Parameter если customer_calls empty
+when "seeds_for_comparison_related"
+  Dir[Rails.root.join("db/seeds/seeds_for_comparison_related.rb")].each { |f| require f }
+end
 
 #to set correct id for new records
 ActiveRecord::Base.connection.tables.each do |t|
