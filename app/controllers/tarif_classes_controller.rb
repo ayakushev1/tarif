d8942d7bcb021b2cb37a15531dd259893a2bb78c
ayakushev1 <ayakushev@rambler.ru>
2016-01-27 23:23:19 +0300

    add redirect from old tarif_class links

--- app/controllers/tarif_classes_controller.rb
+++ app/controllers/tarif_classes_controller.rb
@@ -4,6 +4,15 @@ class TarifClassesController < ApplicationController
   include Crudable
   crudable_actions :all
   
+  before_filter :check_before_freindly_url, only: [:show]
+  
+  def check_before_freindly_url
+    @tarif_class = TarifClass.where(:id => params[:id]).first
+    if @tarif_class and request.path != tarif_class_path(@tarif_class)
+      redirect_to tarif_class_path(@tarif_class), :status => :moved_permanently
+    end if params[:id]
+  end
+  
 
 
 end
