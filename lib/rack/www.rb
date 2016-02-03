1a3cf47a68bd4d0ef2b6876e8bbbef690aba9bd8
ayakushev1 <ayakushev@rambler.ru>
2016-02-04 01:06:16 +0300

    add redirect to www from blank site name

---
+++ lib/rack/www.rb
@@ -0,0 +1,15 @@
+module Rack
+  class Www
+    def initialize(app)
+      @app = app
+    end
+    def call(env)
+      if (env['SERVER_NAME'] =~ /^www\./) or (env['SERVER_NAME'] == '46.101.146.117')
+        @app.call(env)
+      else
+        @app.call(env)
+#        [ 301, { 'Location' => 'http://www.mytarifs.ru/' }, '' ]
+      end
+    end
+  end
+end
