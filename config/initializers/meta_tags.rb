b39734a77a567edb1405c03dce0a9feb07ee5910
ayakushev1 <ayakushev@rambler.ru>
2016-02-05 05:28:22 +0300

    add metatags

--- config/initializers/meta_tags.rb
+++ config/initializers/meta_tags.rb
@@ -1,6 +1,6 @@
 MetaTags.configure do |c|
   c.title_limit        = 700
-  c.description_limit  = 160
-  c.keywords_limit     = 255
+  c.description_limit  = 1600
+  c.keywords_limit     = 2550
   c.keywords_separator = ', '
 end
